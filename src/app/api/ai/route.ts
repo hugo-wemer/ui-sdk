04990710b56e918c65109ec66cf51110850b8a2d
hugowemer <wemer.eng@gmail.com>
2025-06-07 19:16:21 -0300

    feat: tool calling

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { generateObject, generateText } from 'ai';
+import { generateObject, generateText, tool } from 'ai';
 import { openrouter } from "@/ai/open-router";
 import { z } from "zod";
 
@@ -15,18 +15,56 @@ export async function GET(request: NextRequest){
 
   // ------------------------------------
 
-  const result = await generateObject({
+  // const result = await generateObject({
+  //   model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+  //   schema: z.object({
+  //     pt: z.string().describe('Tradução para português'),
+  //     fr: z.string().describe('Tradução para francês'),
+  //     es: z.string().describe('Tradução para espanhol')
+  //   }),
+  //   prompt: 'Traduza "Hello World!" para diferentes idiomas.',
+  //   system: 'você é uma AI especializada em tradução, sempre retorne da maneira mais sucinta possível'
+  // })
+
+  // ------------------------------------
+
+  const result = await generateText({
     model: openrouter.chat('openai/gpt-4o-2024-11-20'),
-    schema: z.object({
-      pt: z.string().describe('Tradução para português'),
-      fr: z.string().describe('Tradução para francês'),
-      es: z.string().describe('Tradução para espanhol')
-    }),
-    prompt: 'Traduza "Hello World!" para diferentes idiomas.',
-    system: 'você é uma AI especializada em tradução, sempre retorne da maneira mais sucinta possível'
+    tools: {
+      profileAndUrls: tool({
+        description: 'Essa ferramenta serve para buscar dados de um usuário no github ou acessar URLs da API para outras informações de um usuário como lista de organizações, repositórios,, eventos, seguidores etc.',
+        parameters: z.object({
+          username: z.string().describe('username do usuário no github')
+        }),
+        execute: async ({ username }) => {
+          const response = await fetch(`https://api.github.com/users/${username}`)
+          const data = await response.json()
+          return JSON.stringify(data)
+        }
+      }),
+
+      fetchHTTP: tool({
+        description: 'Essa ferramenta serve para realizar um requisição HTTP em uma URL especificada e acessar sua resposta.',
+        parameters: z.object({
+          url: z.string().url().describe('URL a ser requisitada')
+        }),
+        execute: async ({ url }) => {
+          const response = await fetch(url)
+          const data = await response.text()
+          return data
+        }
+      })
+    },
+    prompt: 'Me dê uma lista de usuários que seguem o hugo-wemer no Github?',
+    maxSteps: 5,
+
+    onStepFinish({ toolResults }){
+      console.log(toolResults)
+    }
   })
 
   return NextResponse.json({
-    message: result.object
+    message: result.text,
+    parts: result.toolCalls
   })
 }
